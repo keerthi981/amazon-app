aaad9b368f5ce72338658d623e80b9e8a1046d03
Keerthipriya <keerthipriya1998.y@gmail.com>
2022-03-23 14:15:45 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9uvW3RALX1lbbbBKsAS530sS7BWuDOWk",
+  authDomain: "fir-b7390.firebaseapp.com",
+  projectId: "fir-b7390",
+  storageBucket: "fir-b7390.appspot.com",
+  messagingSenderId: "554393352487",
+  appId: "1:554393352487:web:cb8d9149c52426133ed102",
+  measurementId: "G-W390RSCHQC"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+var db = firebase.firestore();
+ 
+
+
+
+
+
+
